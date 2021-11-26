3c7c65498cc1d3e42d8598d50fd8c6cf057fe263
Reyna Royce <80285415+reynaroyce12@users.noreply.github.com>
2021-11-26 20:42:09 +0530

    Update main.py

--- goodmorningwisher/main.py
+++ goodmorningwisher/main.py
@@ -2,8 +2,8 @@ from smtplib import *
 from datetime import *
 import random
 
-my_email = "gilfoylethegoldfoil@gmail.com"
-my_password = "testermail12!"
+my_email = '***************@gmail.com'
+my_password = '***********'
 
 current_day = datetime.today().weekday()
 
@@ -15,7 +15,7 @@ if current_day == 0:
         connection.starttls()
         connection.login(user=my_email, password=my_password)
         connection.sendmail(from_addr=my_email,
-                            to_addrs="reynaroyce.12@gmail.com",
+                            to_addrs="*************@gmail.com",
                             msg=f"Subject:Some Monday motivation\n\n"f"It's the start of a new week!\n{random_quote}\nGood day!")
 else:
     with open('quotes.txt') as file:
@@ -25,5 +25,5 @@ else:
         connection.starttls()
         connection.login(user=my_email, password=my_password)
         connection.sendmail(from_addr=my_email,
-                            to_addrs="reynaroyce.12@gmail.com",
+                            to_addrs="**************@gmail.com",
                             msg=f"Subject:Today's quote!\n\n"f"\n{random_quote}\nGood day!")
